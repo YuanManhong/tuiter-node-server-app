aad46ff7a53526247f30c3e6b5886d06b86d3a73
Manhong Yuan <Yuan.Manh@northeastern.edu>
2023-04-01 16:41:59 -0400

    first commit

---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,53 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const TuitController = (app) => {
+    app.get('/api/tuits', findTuits);
+    app.post('/api/tuits', createTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+    app.put('/api/tuits/:tid', updateTuit);
+}
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime() + '';
+    newTuit.likes = 0;
+    newTuit.liked = false;
+    tuits.push(newTuit);
+    res.json(newTuit);
+};
+
+const findTuits = (req, res) => {
+    res.json(tuits);
+};
+
+const updateTuit = (req, res) => {
+    const tuitId = req.params.tid;
+    const updates = req.body;
+    tuits = tuits.map (t => t._id === tuitId ? {
+        ...t,
+        ...updates
+    } : t);
+    res.sendStatus(200);
+};
+
+/*
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex(
+        (t) => t._id === tuitdIdToUpdate)
+    tuits[tuitIndex] =
+        {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+}
+*/
+
+
+const deleteTuit = (req, res) => {
+    const tuitId = req.params.tid;
+    tuits = tuits.filter(t => t._id !== tuitId);
+    res.sendStatus(200);
+};
+
+export default TuitController;
